b733f2e85ca208844cfad426dd228e1a23affb46
shreyans264 <shreyans.264@gmail.com>
2015-06-16 15:21:25 +0530

    composer added

--- createComment.php
+++ createComment.php
@@ -4,7 +4,7 @@ class createComment
 	public function Comment($msg, $line, $filename, $owner, $repository, $number, $id) {
 	
 		echo $msg . " : " . $line . " : " . $filename;
-		require_once(__DIR__ . '/github-php-client/client/GitHubClient.php');
+		require_once(__DIR__ . '/vendor/tan-tan-kanarek/github-php-client/client/GitHubClient.php');
 		$config = include "config.php";	
 		$filename = str_replace("\ "," ", $filename);
 		$client = new GitHubClient();
