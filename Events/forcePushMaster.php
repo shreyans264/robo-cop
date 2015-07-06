49f84b1c2148fc8b39c36cf098b3d8a7a98ec628
shreyans264 <shreyans.264@gmail.com>
2015-07-06 17:32:57 +0530

    force push only for master

--- Events/forcePushMaster.php
+++ Events/forcePushMaster.php
@@ -4,14 +4,13 @@ class forcedPushed
 	public function findEnforcer($load)
 	{
 		echo " forced push";
-		//if($load["branch"] == "master")
-		//{
+		if($load["branch"] == "master")
+		{
 			echo " in master";
 			echo " " . $load["branch"];
-			$msg ="owner: " . $load["owner"] . "\nauthor: " . $load["author"] . "\ncommitter: " . $load["committer"] . "\nrepository: " . $load["repository"] . "\nPushed from Branch: " . $load["branch"];
-			mail($load["owner_email"],"[Robo-Cop] Someone forced pushed",$msg);
-			mail($load["author_email"],"[Robo-Cop] Someone forced pushed in your repo",$msg);
-		//}
+			$msg ="owner: " . $load["owner"] . "\nauthor: " . $load["author"] . "\ncommitter: " . $load["committer"] . "\nrepository: " . $load["repository"] . "\nBranch: " . $load["branch"];
+			mail("ray.dev@practo.com","[Robo-Cop] Someone forced pushed",$msg);
+		}
 	}
 }
 
